979c458df5891a76d7e1537252f61ffe4cefb9d3
Maks <maks.service@protonmail.com>
2021-11-29 15:31:26 +0100

    fix: compiling with strict tsconfig

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 declare module 'mongoose-lean-getters' {
   import mongoose = require('mongoose');
 
-  export default function mongooseLeanGetters(schema: mongoose.Schema); 
+  export default function mongooseLeanGetters(schema: mongoose.Schema): void;
 }
